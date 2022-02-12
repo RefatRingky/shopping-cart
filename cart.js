d0aeb4fbefc175c4b517640c2e53d07ef41fade1
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-13 02:15:57 +0600

    second message

--- cart.js
+++ cart.js
@@ -1,6 +1,6 @@
 // use function for shopping cart
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById("case-number");
+function updateCaseNumber( product,price,isIncreasing) {
+    const caseInput = document.getElementById(product+'-number');
     let caseNumber = caseInput.value;
     if(isIncreasing == true){
         caseNumber = parseInt(caseNumber)+ 1;  
@@ -10,21 +10,28 @@ function updateCaseNumber(isIncreasing) {
     }
     caseInput.value = caseNumber;
     // for increasing the value
-    const caseTotal = document.getElementById("case-total");
-    caseTotal.innerText = caseNumber * 59 ; 
+    const caseTotal = document.getElementById(product+ '-total');
+    caseTotal.innerText = caseNumber * price ; 
 }
 
 // evenListener for plus button
 document.getElementById("case-plus").addEventListener('click',function () {
-   updateCaseNumber(true);
+   updateCaseNumber('case',59, true);
 });
 
 // evenlistener for minus button
 document.getElementById("case-minus").addEventListener('click',function () {
-    updateCaseNumber(false);
+    updateCaseNumber('case',59, false);
 });
 
-// shopping cart
+// phone increase decrease 
+document.getElementById("phone-plus").addEventListener('click', function name(params) {
+    updateCaseNumber('phone',1219,true);
+});
+// for minus
+document.getElementById("phone-minus").addEventListener('click', function name(params) {
+    updateCaseNumber('phone',1219,false);
+});
 
 
 
